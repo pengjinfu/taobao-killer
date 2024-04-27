a97be01c31879a2e8ecf5715a05e2fa7e636cca3
cap153 <67049883+cap153@users.noreply.github.com>
2024-04-27 11:56:17 +0800

    实现间隔一分钟刷新页面防止掉登录

--- taobao_killer.py
+++ taobao_killer.py
@@ -5,7 +5,7 @@ import datetime
 page = ChromiumPage()
 
 # 指定秒杀时间
-kill_time = "2024-04-18 15:19:00.00000000"
+kill_time = "2024-04-27 11:53:00.00000000"
 
 # 打开淘宝网页
 page.get("https://www.taobao.com")
@@ -19,12 +19,18 @@ page.ele('x://*[@id="J_SelectAll1"]/div/label').click()
 while(True):
     # 获取当前时间
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    print(now)
+    print(now) # 打印当前时间测试
     # 判断当前时间是否到达了秒杀时间
     if(now>kill_time):
         # 点击结算按钮
         page.ele('x://*[@id="J_SmallSubmit"]').click()
         break
+    # 判断当前秒数是不是0，实现间隔一分钟刷新页面，防止掉登录
+    if(datetime.datetime.now().second == 0):
+        page.refresh() # DrissionPage的页面刷新方法，内置了wait.load_start()程序会自动等待加载结束
+        # 再次全选购物车
+        page.ele('x://*[@id="J_SelectAll1"]/div/label').click()
+
 
 # 测试时的程序暂停
 input()
